33e5f4175d105831a9f157ce1e28934f81a1f533
Paweł Pańczak <p.panczak@onet.eu>
2022-06-16 12:36:14 +0200

    files implementation 1

---
+++ js.js
@@ -0,0 +1,12 @@
+const burgerBtn = document.querySelector('.burger')
+const bardIco = document.querySelector('.fa-bars')
+const xIco = document.querySelector('.fa-times')
+const nav = document.querySelector('nav ul')
+
+const handleNav = () => {
+    nav.classList.toggle('active')
+    burgerBtn.classList.toggle('active')
+    xIco.classList.toggle('hide')
+    bardIco.classList.toggle('hide')
+}
+burgerBtn.addEventListener('click', handleNav)
